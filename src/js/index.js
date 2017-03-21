0fb5c23e889ca12ede7d1180d554d234b3148ca4
Louis DE L'HOMMEAU <E155583M@U-INF-J-F222-10.iut-nantes.univ-nantes.prive>
2017-03-21 16:28:49 +0100

    SUPER COMMIT!

--- src/js/index.js
+++ src/js/index.js
@@ -26,4 +26,24 @@ $(document).ready(function(){
   });
 
   $('#myTable').DataTable();
+
+  $("#rechercher").on('click',  function(event){
+  $("#images").empty();
+  console.log("gclické");
+  $.ajax({
+      url:'http://api.flickr.com/services/feeds/photos_public.gne',
+      type:'GET',
+      dataType:'jsonp',
+      jsonp: 'jsoncallback', // a renseigner d'après la doc du service, par défaut callback
+      data:'tags='+$("#commune").val()+'s&tagmode=any&format=json',
+      success:function(data){
+      $.each(data.items, function(i,item){
+                  $("<img/>").attr("src", item.media.m).appendTo("#images");
+                  if ( i == 6 ) return false ; });
+                },
+      error: function(resultat,statut,erreur){
+      console.log("erreur");},
+       });
+});
+
 });
